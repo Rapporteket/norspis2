3949464706f3031b1979000c0769567a1058ca10
mro046 <mads.kristian.rohde@nordlandssykehuset.no>
2021-01-14 11:11:03 +0100

    Cleaned code (reduced line length)

--- R/make_figFig_unitCompar.R
+++ R/make_figFig_unitCompar.R
@@ -14,13 +14,16 @@
 #'
 #' @examples
 
-make_figFig_unitCompar <- function(my_proptable_hospitals,
-                                   my_y_lab = "Andel (%)", #default "Andel (%), unless you change it
-                                   my_title = '',#default empty (""), unless you change it
-                                   YellowGoal = 'mean',  #can be give the values 1)'' 2)'mean' or 3) A character value which will be converted to na , for instance '60' if the goal value is 60 %
-                                   GreenGoal = 'mean'){
+make_figFig_unitCompar <- function(
+  my_proptable_hospitals,
+  my_y_lab = "Andel (%)", #default "Andel (%), unless you change it
+  my_title = '',#default empty (""), unless you change it
+  YellowGoal = 'mean',  #1)'' 2)'mean' or 3) for instance '60'
+  GreenGoal = 'mean')
+  {
   #Theme:
-  if(!exists("skriftstorleik")) # Skriftstorleik b?r vera definert i kvar ?rsrapportfil
+  if(!exists("skriftstorleik")) # Skriftstørrelse bør være definert
+                                #i kvarrtalsrapportfil
     skriftstorleik = 13
   tema = ggplot2::theme_light(base_size=skriftstorleik)
   tema$panel.grid.minor$colour="white"
@@ -47,37 +50,49 @@ make_figFig_unitCompar <- function(my_proptable_hospitals,
     ggplot2::ggplot(my_proptable_hospitals)+
     #The bars:
     ggplot2::geom_col(mapping=ggplot2::aes(x = AvdNavn, y=perc),#,width=1/3
-             fill=factor(ifelse(stringr::str_detect(my_proptable_hospitals$AvdNavn, "Nasjonal"),"grey", "#2171b5"))) +
+             fill=factor(ifelse(stringr::str_detect(
+               my_proptable_hospitals$AvdNavn,
+               "Nasjonal"),
+               "grey", "#2171b5"))) +
 
     ggplot2::labs(y=my_y_lab, #my_proptable_hospitals$my_y_lab,#
-         title = my_title)+#colnames(my_proptable_hospitals[,4]))+ #my_figText$title)+
-    ggplot2::scale_y_continuous(limits = c(0,max(my_proptable_hospitals$perc)), #c(0,105)
-                       expand = ggplot2::expansion(mult=c(0,0.1)),breaks = seq(0,100,10))+ #in "expansion use "add" instead of "mult" to make expansion abolute rather than relative(mult for multiplication)
+         title = my_title)+#colnames(my_proptable_hospitals[,4]))+
+                           #my_figText$title)+
+    ggplot2::scale_y_continuous(
+      limits = c(0,max(my_proptable_hospitals$perc)), #c(0,105)
+      expand = ggplot2::expansion(mult=c(0,0.1)),breaks = seq(0,100,10))+
+      #above, in "expansion use "add" instead of "mult" to make expansion
+      #absolute rather than relative(mult for multiplication)
     ggplot2::xlab(NULL)+
-    # geom_errorbar(aes(x=my_proptable_hospitals$AvdNavn, ymin=my_proptable_hospitals$perc-sd(my_proptable_hospitals$perc),
-    #                                                 ymax=my_proptable_hospitals$perc+sd(my_proptable_hospitals$perc))
+    # geom_errorbar(aes(x=my_proptable_hospitals$AvdNavn,
+    # ymin=my_proptable_hospitals$perc-sd(my_proptable_hospitals$perc),
+    # ymax=my_proptable_hospitals$perc+sd(my_proptable_hospitals$perc))
     #               , colour= 'orange',alpha=0.5, size=1, width=0.2)+
     ggplot2::geom_text(
       ggplot2::aes(x = AvdNavn, y= 1,
-          label= factor(ifelse(perc == 0.00000123,'',paste0(round(perc,1)))), #ifelse statement to remove 0% values (if you want with "%", use: paste0(round(perc,1),'%') #to map percentace label at top of bar use "y= perc"
+          label= factor(ifelse(perc == 0.00000123,'',paste0(round(perc,1)))),
+          #ifelse statement to remove 0% values (if you want with "%", use:
+          #paste0(round(perc,1),'%')
+          #to map percentace label at top of bar use "y= perc"
           hjust='left'),
       alpha=0.5)+
 
     ggplot2::geom_text(
       ggplot2::aes(x = AvdNavn, y= 1,
-          label= factor(ifelse(n < 5,"N<5",'')), #ifelse statement to add text saying n is lower than 5
+          label= factor(ifelse(n < 5,"N<5",'')), #ifelse statement to add
+                                                #text saying n is lower than 5
           hjust='left'),
       alpha=0.5)+
 
     # geom_text(
     #   aes(x = AvdNavn, y= 100,
-    #       label=n_missing, #to map percentace label at top of bar use "y= perc"
+    #     label=n_missing, #to map percentace label at top of bar use "y= perc"
     #       hjust='left'),
     #   alpha=0.5)+
 
     # geom_text(
     #   aes(x = AvdNavn, y= 105,
-    #       label=n_uaktuell, #to map percentace label at top of bar use "y= perc"
+    #   label=n_uaktuell, #to map percentace label at top of bar use "y= perc"
     #       hjust='left'),
   #   alpha=0.5)+
 
@@ -85,7 +100,8 @@ make_figFig_unitCompar <- function(my_proptable_hospitals,
   #geom_text(
   #  aes(x = AvdNavn, y= -0.15, label=paste0('N=(',n,')')),
   #  hjust='left')+
-  #scale_y_continuous(limits = c(-0.15,1))+ # - 0,15 to increase margin below/to the left of bar to make space for N
+  #scale_y_continuous(limits = c(-0.15,1))+ # - 0,15 to increase margin
+  #below/to the left of bar to make space for N
   tema +
     fjern_y +
     ggplot2::theme(axis.ticks.y = ggplot2::element_blank())+
@@ -95,22 +111,41 @@ make_figFig_unitCompar <- function(my_proptable_hospitals,
   if (GreenGoal!=''){
     if(GreenGoal=='mean'){
       fig <-   fig +
-        ggplot2::geom_hline(yintercept = my_proptable_hospitals[stringr::str_detect(my_proptable_hospitals$AvdNavn, "Nasjonal"),]$perc, col='forestgreen', size=1, linetype=1) #to set goal value at national value: "my_proptable_hospitals[stringr::str_detect(my_proptable_hospitals$AvdNavn, "Nasjonal"),]$perc"
+        ggplot2::geom_hline(
+          yintercept = my_proptable_hospitals[stringr::str_detect(
+            my_proptable_hospitals$AvdNavn, "Nasjonal"),]$perc,
+          col='forestgreen',
+          size=1,
+          linetype=1) #to set goal value at national value: "my_proptable_hospitals[stringr::str_detect(my_proptable_hospitals$AvdNavn, "Nasjonal"),]$perc"
 
     } else {
       fig <-   fig +
-        ggplot2::geom_hline(yintercept = as.numeric(GreenGoal), col='forestgreen', size=1, linetype=1)
+        ggplot2::geom_hline(yintercept = as.numeric(GreenGoal),
+                            col='forestgreen',
+                            size=1,
+                            linetype=1)
     }
   }
   # yellow goal line:
   if (YellowGoal!=''){
     if(YellowGoal=='mean'){
       fig <-   fig +
-        ggplot2::geom_hline(yintercept = my_proptable_hospitals[stringr::str_detect(my_proptable_hospitals$AvdNavn, "Nasjonal"),]$perc, col='gold', size=1, linetype=1) #to set goal value at national value: "my_proptable_hospitals[stringr::str_detect(my_proptable_hospitals$AvdNavn, "Nasjonal"),]$perc"
+        ggplot2::geom_hline(
+          yintercept = my_proptable_hospitals[stringr::str_detect(
+            my_proptable_hospitals$AvdNavn, "Nasjonal"),]$perc,
+          col='gold',
+          size=1,
+          linetype=1) #to set goal value at national value:
+      #"my_proptable_hospitals[stringr::str_detect(
+      #my_proptable_hospitals$AvdNavn, "Nasjonal"),]$perc"
 
     } else {
     fig <-   fig +
-      ggplot2::geom_hline(yintercept = as.numeric(YellowGoal), col='gold', size=1, linetype=1) #
+        ggplot2::geom_hline(
+          yintercept = as.numeric(YellowGoal),
+          col='gold',
+          size=1,
+          linetype=1) #
     }
 
   }

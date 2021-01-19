23481f69bb3f20f47cb043bb326446c7ba364596
Are Edvardsen <biorakel@gmail.com>
2021-01-19 21:28:20 +0100

    odd function def

--- inst/shinyApps/norspis2/server.R
+++ inst/shinyApps/norspis2/server.R
@@ -1,6 +1,6 @@
 library(shiny)
 
-shinyServer(function(input, output, session) {
+server <- function(input, output, session) {
 
   # Hide/show tabs
   #hideTab(inputId = "tabs", target = "Sykehussammenligninger")
